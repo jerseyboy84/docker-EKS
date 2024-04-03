ebb990640b0747f3ceff2dbfc933513cdc49d72c
Brian Rockermann <brian.rockermann@gmail.com>
2024-04-02 20:39:17 -0700

    updated Healthy message

--- test_main.py
+++ test_main.py
@@ -25,8 +25,7 @@ def client():
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json == 'Healthy'
-    assert False
+    assert response.json == 'Healthy running locally and on EKS'
 
 
 def test_auth(client):
